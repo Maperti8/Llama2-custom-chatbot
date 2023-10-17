c355a0b5bbcb405088f286d58ce4aafc537cfcc7
Martin-Pekny <martin.pekny7@gmail.com>
2023-10-17 22:40:28 +0200

    !feature: added about module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', loadChildren: () => import('./modules/about-module/about-module.module').then(m => m.AboutModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
