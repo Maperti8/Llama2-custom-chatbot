6dc17e51e91e40c76a18d3ae81ff886e1b77f3d3
Martin-Pekny <martin.pekny7@gmail.com>
2023-10-18 20:12:14 +0200

    feat: base form setup for sending messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/components/home/home.component';
 
 const routes: Routes = [
+  // eager loaded components
+  { path: '', redirectTo: 'chatbot', pathMatch: 'full'},
+  { path: 'chatbot', component: HomeComponent },
+  // lazy loaded modules
   { path: 'about', loadChildren: () => import('./modules/about-module/about-module.module').then(m => m.AboutModule) },
 ];
 
--- src/app/modules/material/material-module.ts
+++ src/app/modules/material/material-module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
+
+
+const materialComponents = [
+  FormsModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatCardModule,
+]
 
 @NgModule({
   imports: [
-
+    materialComponents
   ],
   exports: [
-    
+    materialComponents
   ]
 })
 export class MaterialModule {}
